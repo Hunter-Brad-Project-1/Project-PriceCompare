7a100ff58217498b73dc30749a1a8daa5377df3c
bradyawn <38081482+bradyawn@users.noreply.github.com>
2020-06-01 15:02:00 -0400

    Update EbayScrapper.py

--- EbayScrapper.py
+++ EbayScrapper.py
@@ -2,6 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
+def process(string):
+    index = -1
+    if string == "":
+        return 0
+    for i in range(0, len(string)-1):
+        if string[i] == ' ':
+            index = i
+    return float(string[index+2:])
+
 class Product:
     name = ""
     price = 0.0
@@ -27,18 +36,35 @@ countGood = 0
 
 print(len(listProducts))
 
+Products = []
+
 for i in range(0, len(listProducts)):
     bookListing = listProducts[i]
     if not bookListing.find(class_="s-item__title--tagblock"):
         #print("I found a good one!")
         #print(bookListing.prettify())
-        print(bookListing.find(class_="s-item__title").text) #Book Name
-        print(bookListing.find(class_="s-item__price").text) #Book Price
-        print(bookListing.find(class_="s-item__shipping s-item__logisticsCost").text) #Shipping Price
+
+        bookName = bookListing.find(class_="s-item__title").text
+
+        bookPrice = 0
+        if bookListing.find(class_="s-item__price").text[-1:] != "g":
+            bookPrice = process(bookListing.find(class_="s-item__price").text)
+
+        shippingPrice = 0
+        if bookListing.find(class_="s-item__shipping s-item__logisticsCost").text[-1:] != "g":
+           shippingPrice = process(bookListing.find(class_="s-item__shipping s-item__logisticsCost").text)            
+
+        Products.append(Product(bookName, bookPrice+shippingPrice))
+        
         countGood += 1
     if countGood == 10:
         break
-    print("")
+
+Products = sorted(Products)
+
+for i in Products:
+    print("" + str(i.name) + " at " + str(i.price))
+
 
 
 
